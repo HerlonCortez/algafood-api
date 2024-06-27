84358090852576f6f53b0620427564b7ddce4f71
herloncortez <herlon.cortez@grupobrisanet.com.br>
2024-06-27 08:07:08 -0300

    adicionando notificador - spring profiles

---
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.di.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificacao.NivelUrgencia;
+import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
+
+@Component
+public class AtivacaoClienteService {
+
+	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
+	@Autowired
+	private Notificador notificador;
+	
+	public AtivacaoClienteService(Notificador notificador) {
+		this.notificador = notificador;
+	}
+	
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+		
+		this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+}
