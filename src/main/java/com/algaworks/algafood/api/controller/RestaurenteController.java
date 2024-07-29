2146c7512ce76e6d58a4408c4fdd0be7275f5552
herloncortez <herlon.cortez@grupobrisanet.com.br>
2024-07-29 07:59:05 -0300

    aulas

--- src/main/java/com/algaworks/algafood/api/controller/RestaurenteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestaurenteController.java
@@ -2,12 +2,15 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +47,18 @@ public class RestaurenteController {
 		return ResponseEntity.notFound().build();
 	}
 	
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<Restaurante> alterar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante ){
+		Restaurante restauranteNovo = restauranteRepository.buscar(restauranteId);
+		if (restauranteNovo != null) {
+			BeanUtils.copyProperties(restaurante, restauranteNovo, "id");
+			cadastroRestauranteService.salvar(restauranteNovo);
+			return ResponseEntity.ok(restauranteNovo);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
 	@PostMapping
 	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
 		try {
@@ -54,6 +69,14 @@ public class RestaurenteController {
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest()
 					.body(e.getMessage());
+		}
+	}
+	
+	@DeleteMapping("/{restauranteId}")
+	public void remover(@PathVariable Long restauranteId) {
+		Restaurante restaurante = restauranteRepository.buscar(restauranteId);
+		if (restaurante != null) {
+			restauranteRepository.remover(restaurante);
 		}
 	}
 }
